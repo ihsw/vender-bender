6179d4e6cfd4afc5a8a65ab552287ac5a5cd0b04
Adrian Parker <iamhiddensomewhere@gmail.com>
2018-03-09 20:34:00 -0500

    Fix spacing.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,15 +1,15 @@
 import { REFUND_CHANGE } from '../constants';
 
 export interface RefundChangeAction {
-    type: REFUND_CHANGE;
-    amount: number;
+  type: REFUND_CHANGE;
+  amount: number;
 }
 
 export const refundChange = (amount: number): RefundChangeAction => {
-    return {
-        type: REFUND_CHANGE,
-        amount
-    };
+  return {
+    type: REFUND_CHANGE,
+    amount
+  };
 };
 
 export type VenderBenderAction = RefundChangeAction;
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,13 +3,13 @@ import { REFUND_CHANGE } from '../constants';
 import { StoreState } from '../types';
 
 export const vendors = (state: StoreState, action: VenderBenderAction): StoreState => {
-    switch (action.type) {
-        case REFUND_CHANGE:
-            return {
-                ...state,
-                changeRefunded: action.amount
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case REFUND_CHANGE:
+      return {
+        ...state,
+        changeRefunded: action.amount
+      };
+    default:
+      return state;
+  }
 };
